ba3ec6244e959ceb2ed3a6b1d8a1fd43b9440551
fabiankachlock <fabian.kachlock@icloud.com>
2023-10-22 01:03:21 +0200

    dargan shop implemtation

--- player/PlayerAutoload.gd
+++ player/PlayerAutoload.gd
@@ -5,7 +5,7 @@ signal player_max_speed_changed(speed: int)
 signal player_stealth_factor_changed(stealth_factor: int)
 
 var candela : int = 0 : set = on_candela_set
-var lux : int = 0
+var lux : int = 400
 
 const max_candela : int = 100
 
@@ -30,3 +30,30 @@ func on_player_max_speed_changed(value) -> void:
 func on_player_stealth_factor_changed(value) -> void:
 	player_stealth_factor = value
 	emit_signal("player_stealth_factor_changed", value)
+
+
+var stealth_lvl = 0 : set = on_player_stealth_lvl_set
+var stealth_max_lvl = 3
+var stealth_upgrades = {
+	0: 1,
+	1: 0.8,
+	2: 0.6,
+	3: 0.4
+}
+
+func on_player_stealth_lvl_set(value):
+	stealth_lvl = value
+	player_stealth_factor = stealth_upgrades[value]
+
+var shoes_lvl = 0 : set = on_player_shoes_lvl_set
+var shoes_max_lvl = 3
+var shoes_upgrades = {
+	0: 500,
+	1: 1000,
+	2: 2000,
+	3: 3000
+}
+
+func on_player_shoes_lvl_set(value):
+	shoes_lvl = value
+	player_max_speed = shoes_upgrades[value]
--- world/village/shop_dargan.gd
+++ world/village/shop_dargan.gd
@@ -1,13 +1,69 @@
 extends Node2D
 
+var regex = RegEx.new()
+
+var stealth_name = "Stealth Cape"
+var stealth_prices = {
+	1: 40,
+	2: 50,
+	3: 60
+}
+
+var shoes_name = "Faster Shoes"
+var shoe_prices = {
+	1: 40,
+	2: 50,
+	3: 60
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	regex.compile("{{.*?}}")
 	Global.connect("dialog_branch_selected", branch_selected)
 	pass # Replace with function body.
 
+
+func _replace_vars(str: String):
+	for result in regex.search_all(str):
+		str = str.replace(result.get_string(), "")
+	return str
+
+func _format_item(name: String, lux: int, lvl: int):
+	return "%s %s(%d Lux)" % [name, "Lvl %d " % lvl if lvl > 1 else "", lux]
+
 func branch_selected(branch: Array):
-	print(branch)
+	var canBuyShoes = PlayerAutoload.shoes_lvl < PlayerAutoload.shoes_max_lvl
+	var value = branch[0]["value"]
+	
+	if value == "{{shop2_item1}}":
+		_upgrade_stealth()
+	elif value == "{{shop2_item2}}":
+		_upgrade_shoes()
+	elif value == "{{shop1_item1}}" && canBuyShoes:
+		_upgrade_shoes()
+	elif value == "{{shop1_item1}}" && !canBuyShoes:
+		_upgrade_stealth()
 	pass
+
+func _upgrade_shoes():
+	var shoes_lvl = PlayerAutoload.shoes_lvl+1
+	if _buy_with_lux(shoe_prices[shoes_lvl], shoes_name):
+		PlayerAutoload.shoes_lvl += 1
+
+func _upgrade_stealth():
+	var stealth_lvl = PlayerAutoload.stealth_lvl+1
+	if _buy_with_lux(stealth_prices[stealth_lvl], stealth_name):
+		PlayerAutoload.stealth_lvl += 1
+
+func _buy_with_lux(amount: int, item_name: String):
+	if PlayerAutoload.lux < amount:
+		Global.DialogOverlay.show_dialog("shop_dargan_missing_lux", {
+			"item": _replace_vars(item_name)
+		})
+		return false
+	else:
+		PlayerAutoload.lux -= amount
+		return true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,8 +71,28 @@ func _process(delta):
 
 
 func _on_area_2d_area_entered(area):
-	Global.DialogOverlay.show_dialog("shop_dargan", {
-		"price1": "ABC",
-		"price2": "EFG",
-		"test": "WHAT",
-	})
+	var canBuyShoes = PlayerAutoload.shoes_lvl < PlayerAutoload.shoes_max_lvl
+	var canBuyStealth = PlayerAutoload.stealth_lvl < PlayerAutoload.stealth_max_lvl
+	var stealth_lvl = PlayerAutoload.stealth_lvl+1
+	var shoes_lvl = PlayerAutoload.shoes_lvl+1
+	
+	if canBuyShoes && canBuyStealth:
+		Global.DialogOverlay.show_dialog("shop_dargan_buy_items_2", {
+			"shop2_item1": _format_item(stealth_name, stealth_prices[stealth_lvl], stealth_lvl),
+			"shop2_item2": _format_item(shoes_name, shoe_prices[shoes_lvl], shoes_lvl),
+			"lux_amount": PlayerAutoload.lux,
+		})
+	elif canBuyShoes:
+		Global.DialogOverlay.show_dialog("shop_dargan_buy_items_1", {
+			"shop1_item1": _format_item(shoes_name, shoe_prices[shoes_lvl], shoes_lvl),
+			"lux_amount": PlayerAutoload.lux,
+		})
+	elif canBuyStealth:
+		Global.DialogOverlay.show_dialog("shop_dargan_buy_items_1", {
+			"shop1_item1": _format_item(stealth_name, stealth_prices[stealth_lvl], stealth_lvl),
+			"lux_amount": PlayerAutoload.lux,
+		})
+	else:
+		Global.DialogOverlay.show_dialog("shop_dargan_no_items", {
+			"lux_amount": PlayerAutoload.lux,
+		})
